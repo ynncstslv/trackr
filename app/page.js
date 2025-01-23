9328dcbc2fe06146cc14108231289b4e5a61c5d2
ynncstslv <ynncstslv@gmail.com>
2025-01-23 09:49:18 -0300

    feat: add all shadcn components

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
-	return <div>Initial Setup</div>;
+	return <Button variant="destructive">DELETE</Button>;
 }
