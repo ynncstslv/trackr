d52aa06493ddbf0c8cf04f8add539334fb0b70f2
ynncstslv <ynncstslv@gmail.com>
2025-01-23 10:49:18 -0300

    feat: setup clerk for auth

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
+import { ClerkProvider } from '@clerk/nextjs';
 
 export const metadata = {
 	title: 'Trackr | Budget Tracking Made Easy',
@@ -13,12 +14,16 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className} text-white antialiased bg-gray-950`}>
-				<Header />
-				<main className="min-h-screen">{children}</main>
-				<Footer />
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body
+					className={`${inter.className} text-white antialiased bg-gray-950`}
+				>
+					<Header />
+					<main className="min-h-screen">{children}</main>
+					<Footer />
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,16 @@
 import Logo from '@/components/ui/Logo';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function Header() {
 	return (
 		<div className="container">
 			<Logo />
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
 		</div>
 	);
 }
